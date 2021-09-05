c5b94c93cd48d270fb63a8f6890926dd84dd27c1
Stefan Pflueger <stefan.pflueger@exxcellent.de>
2021-09-05 23:51:36 +0200

    feat: add jwt auth and cors to backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,10 @@ from django.urls import path, include
 from django.contrib.auth.models import User
 from django.contrib import admin
 from rest_framework import routers, serializers, viewsets
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -40,5 +44,6 @@ router.register(r"users", UserViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
